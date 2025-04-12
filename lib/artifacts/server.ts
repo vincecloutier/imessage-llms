97cbf0122bd30eab0697113697b3c874b03154dd
vincecloutier <94201611+vincecloutier@users.noreply.github.com>
2025-04-12 15:43:15 -0400

    removed some stuff

--- lib/artifacts/server.ts
+++ lib/artifacts/server.ts
@@ -1,6 +1,4 @@
-import { codeDocumentHandler } from '@/artifacts/code/server';
 import { imageDocumentHandler } from '@/artifacts/image/server';
-import { sheetDocumentHandler } from '@/artifacts/sheet/server';
 import { textDocumentHandler } from '@/artifacts/text/server';
 import { ArtifactKind } from '@/components/artifact';
 import { DataStreamWriter } from 'ai';
@@ -91,9 +89,7 @@ export function createDocumentHandler<T extends ArtifactKind>(config: {
  */
 export const documentHandlersByArtifactKind: Array<DocumentHandler> = [
   textDocumentHandler,
-  codeDocumentHandler,
   imageDocumentHandler,
-  sheetDocumentHandler,
 ];
 
 export const artifactKinds = ['text', 'code', 'image', 'sheet'] as const;
