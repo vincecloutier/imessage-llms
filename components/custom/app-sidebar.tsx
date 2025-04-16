478f70bb9f50deb244538453437fa023c602433f
vincecloutier <94201611+vincecloutier@users.noreply.github.com>
2025-04-16 12:51:40 -0400

    cleaned up file

--- components/custom/app-sidebar.tsx
+++ components/custom/app-sidebar.tsx
@@ -1,22 +1,18 @@
 'use client';
 
 import { User } from '@supabase/supabase-js';
-import { useRouter } from 'next/navigation';
 
 import { SidebarHistory } from '@/components/custom/nav-personas';
 import {
   Sidebar,
   SidebarContent,
-  SidebarGroup,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuItem,
   SidebarMenuButton,
-  useSidebar,
 } from '@/components/ui/sidebar';
 import { BookOpen, Command, LifeBuoy, Send  } from 'lucide-react';
 import { NavSecondary } from '@/components/custom/nav-footer';
-
 
 const data = {
     navSecondary: [
@@ -34,51 +30,11 @@ const data = {
       title: "Privacy Policy",  
       url: "/privacy",
       icon: BookOpen,
-    },
-    // {
-    //   title: "Terms of Service",
-    //   url: "/terms",
-    //   icon: BookOpen,
-    // }
+    }
   ]
 }
-// export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-//   return (
-//     <Sidebar variant="inset" {...props}>
-//       <SidebarHeader>
-//         <SidebarMenu>
-//           <SidebarMenuItem>
-//             <SidebarMenuButton size="lg" asChild>
-//               <a href="#">
-//                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-//                   <Command className="size-4" />
-//                 </div>
-//                 <div className="grid flex-1 text-left text-sm leading-tight">
-//                   <span className="truncate font-semibold">Acme Inc</span>
-//                   <span className="truncate text-xs">Enterprise</span>
-//                 </div>
-//               </a>
-//             </SidebarMenuButton>
-//           </SidebarMenuItem>
-//         </SidebarMenu>
-//       </SidebarHeader>
-//       <SidebarContent>
-//         <NavMain items={data.navMain} />
-//         <NavProjects projects={data.projects} />
-//         <NavSecondary items={data.navSecondary} className="mt-auto" />
-//       </SidebarContent>
-//       <SidebarFooter>
-//         <NavUser user={data.user} />
-//       </SidebarFooter>
-//     </Sidebar>
-//   )
-// }
-
 
 export function AppSidebar({ user, ...props }: { user: User | null } & React.ComponentProps<typeof Sidebar>) {
-  const router = useRouter();
-  const { setOpenMobile } = useSidebar();
-
   return (
     <Sidebar variant="inset" {...props}>
       <SidebarHeader>
@@ -86,7 +42,6 @@ export function AppSidebar({ user, ...props }: { user: User | null } & React.Com
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <div>
-                {/* <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground"> */}
                 <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
                   <Command className="size-4" />
                 </div>
