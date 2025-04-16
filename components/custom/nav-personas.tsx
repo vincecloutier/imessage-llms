e3ea556cf7705319fd4c9bf4d654047c4c5166c6
vincecloutier <94201611+vincecloutier@users.noreply.github.com>
2025-04-16 13:06:00 -0400

    personas nav

--- components/custom/nav-personas.tsx
+++ components/custom/nav-personas.tsx
@@ -13,7 +13,21 @@ import {
   MoreHorizontal,
   Trash2,
   Plus,
+  ChevronRight
 } from 'lucide-react';
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import {
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from "@/components/ui/sidebar"
+
+import { UserDialog } from './user-dialog';
 
 import {
   AlertDialog,
@@ -215,3 +229,64 @@ export function SidebarHistory({ user }: { user: User | undefined }) {
     </>
   );
 }
+
+
+
+// TODO: if you ever want to add memories and assets to be editable, use this as a starting point
+// export function NavMain({
+//   items,
+// }: {
+//   items: {
+//     title: string
+//     url: string
+//     icon: LucideIcon
+//     isActive?: boolean
+//     items?: {
+//       title: string
+//       url: string
+//     }[]
+//   }[]
+// }) {
+//   return (
+//     <SidebarGroup>
+//       <SidebarGroupLabel>Platform</SidebarGroupLabel>
+//       <SidebarMenu>
+//         {items.map((item) => (
+//           <Collapsible key={item.title} asChild defaultOpen={item.isActive}>
+//             <SidebarMenuItem>
+//               <SidebarMenuButton asChild tooltip={item.title}>
+//                 <a href={item.url}>
+//                   <item.icon />
+//                   <span>{item.title}</span>
+//                 </a>
+//               </SidebarMenuButton>
+//               {item.items?.length ? (
+//                 <>
+//                   <CollapsibleTrigger asChild>
+//                     <SidebarMenuAction className="data-[state=open]:rotate-90">
+//                       <ChevronRight />
+//                       <span className="sr-only">Toggle</span>
+//                     </SidebarMenuAction>
+//                   </CollapsibleTrigger>
+//                   <CollapsibleContent>
+//                     <SidebarMenuSub>
+//                       {item.items?.map((subItem) => (
+//                         <SidebarMenuSubItem key={subItem.title}>
+//                           <SidebarMenuSubButton asChild>
+//                             <a href={subItem.url}>
+//                               <span>{subItem.title}</span>
+//                             </a>
+//                           </SidebarMenuSubButton>
+//                         </SidebarMenuSubItem>
+//                       ))}
+//                     </SidebarMenuSub>
+//                   </CollapsibleContent>
+//                 </>
+//               ) : null}
+//             </SidebarMenuItem>
+//           </Collapsible>
+//         ))}
+//       </SidebarMenu>
+//     </SidebarGroup>
+//   )
+// }
