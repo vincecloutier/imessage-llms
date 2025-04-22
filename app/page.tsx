5d497cf71bcc0d3cca26e3b2d3c2c42791c9b8e0
vincecloutier <94201611+vincecloutier@users.noreply.github.com>
2025-04-22 11:47:25 -0400

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Chat as PreviewChat } from '@/components/custom/chat';
 export default async function Home() {  
   return (
       <PreviewChat
+      user_id={null}
       id={null}
       initialMessages={[]}
     />
