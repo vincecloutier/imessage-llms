337580b98f8908757a61194d2d802c18f9036686
vincecloutier <94201611+vincecloutier@users.noreply.github.com>
2025-04-23 11:52:25 -0400

    spacing

--- components/custom/nav-secondary.tsx
+++ components/custom/nav-secondary.tsx
@@ -1,13 +1,7 @@
 import * as React from "react"
 import { type LucideIcon } from "lucide-react"
 
-import {
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar"
+import { SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
 
 export function NavSecondary({items, ...props}: {items: {title: string, url: string, icon: LucideIcon}[]} & React.ComponentPropsWithoutRef<typeof SidebarGroup>) {
   return (
