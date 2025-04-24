18acb26bfbc2faf3f5ca191b62a058ad91b9efd3
vincecloutier <94201611+vincecloutier@users.noreply.github.com>
2025-04-24 07:09:13 -0400

    app header fixed

--- components/custom/app-header.tsx
+++ components/custom/app-header.tsx
@@ -1,23 +1,23 @@
-import { SidebarTrigger } from "../ui/sidebar";
-import { Separator } from "@radix-ui/react-separator";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "../ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 
 export function AppHeader({title, subtitle}: {title: string, subtitle: string})  {
     return (
-    <header className="flex justify-between h-16 shrink-0 items-center gap-2 px-4">
-        <div className="flex items-center gap-2">
-        <SidebarTrigger/>   
-        <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
-        <Breadcrumb>
-            <BreadcrumbList>
-            <BreadcrumbItem className="hidden md:block">{title}</BreadcrumbItem>
-            <BreadcrumbSeparator className="hidden md:block" />
-            <BreadcrumbItem>
-                <BreadcrumbPage>{subtitle}</BreadcrumbPage>
-            </BreadcrumbItem>
-            </BreadcrumbList>
-        </Breadcrumb>
-        </div>
-    </header>
+        <header className="flex h-16 shrink-0 items-center gap-2 px-4">
+            <div className="flex items-center gap-2">
+            <SidebarTrigger/>   
+            <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem className="hidden md:block">{title}</BreadcrumbItem>
+                    <BreadcrumbSeparator className="hidden md:block" />
+                    <BreadcrumbItem>
+                        <BreadcrumbPage>{subtitle}</BreadcrumbPage>
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+            </div>
+        </header>
     )
 }
