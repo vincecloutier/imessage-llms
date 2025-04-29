aba4447b044c6662ac9e9d31cb35a36667c5a651
vincecloutier <94201611+vincecloutier@users.noreply.github.com>
2025-04-29 11:37:49 -0400

    removed weird caching

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,38 +1,37 @@
-import { cache } from 'react';
 import { createClient } from '@/lib/supabase/server';
 
-export const getUser = cache(async () => {
+export const getUser = async () => {
   const supabase = await createClient();
   const {data: { user }, error} = await supabase.auth.getUser();
   if (!user) return null;
   if (error) {console.error(error); return null;}
   return user;
-});
+};
 
-export const getProfile = cache(async (id: string) => {
+export const getProfile = async (id: string) => {
   const supabase = await createClient();
   const { data: users, error } = await supabase.from('profiles').select().eq('id', id).maybeSingle();
   if (error) {console.error(error); return null;}
   return users;
-});
+};
 
-export const getPersonaById = cache(async (personaId: string) => {
+export const getPersonaById = async (personaId: string) => {
   const supabase = await createClient();
   const { data: persona, error } = await supabase.from('personas').select().eq('id', personaId).single();
-  if (error) throw error;
+  if (error) {console.error(error); return null;}
   return persona;
-});
+};
 
-export const getPersonasByUserId = cache(async (userId: string) => {
+export const getPersonasByUserId = async (userId: string) => {
   const supabase = await createClient();
   const { data: personas, error } = await supabase.from('personas').select().eq('user_id', userId);
-  if (error) throw error;
+  if (error) {console.error(error); return [];}
   return personas;
-});
+};
 
-export const getMessagesByPersonaId = cache(async (personaId: string) => {
+export const getMessagesByPersonaId = async (personaId: string) => {
   const supabase = await createClient();
   const { data: messages, error } = await supabase.from('messages').select().eq('persona_id', personaId).order('created_at', { ascending: true }).limit(20);
-  if (error) throw error;
+  if (error) {console.error(error); return [];}
   return messages;
-});
+};
