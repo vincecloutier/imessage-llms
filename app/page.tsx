a59504c131b63379a51ac0a6e25ba3779748b09a
vincecloutier <94201611+vincecloutier@users.noreply.github.com>
2025-05-01 09:40:46 -0400

    explaining redirecting

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 import { Chat } from '@/components/custom/chat-main';
 import { createClient } from '@/lib/supabase/server';
-import { redirect } from 'next/navigation';
+import { redirect, notFound } from 'next/navigation';
 
 export default async function Home() {
   const supabase = await createClient();
   let { user } = (await supabase.auth.getUser()).data;
-  if (!user || user.is_anonymous) {
-    user = (await supabase.auth.signInAnonymously()).data.user;
-  }
-  if (!user) return null;
-  if (!user.is_anonymous) redirect('/chat');
+
+  // if user is signed in, redirect to chat
+  if (user && !user.is_anonymous) redirect('/chat');
+
+  // if user is not signed in, sign in anonymously
+  if (!user) user = (await supabase.auth.signInAnonymously()).data.user;
+
+  // if there is still no user, give up
+  if (!user) return notFound();
 
   return (
     <Chat
