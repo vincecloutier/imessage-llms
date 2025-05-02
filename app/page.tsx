9be043f5fe4abececf2de16718d8f03386594e75
vincecloutier <94201611+vincecloutier@users.noreply.github.com>
2025-05-02 08:44:52 -0400

    forcing redirect to chat/0

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,27 @@
-import { Chat } from '@/components/custom/chat-main';
-import { createClient } from '@/lib/supabase/server';
 import { redirect, notFound } from 'next/navigation';
+
+import { createClient } from '@/lib/supabase/server';
+import { Chat } from '@/components/custom/chat-main';
+import { AppHeader, SignInDialog } from '@/components/custom/app-header';
 
 export default async function Home() {
   const supabase = await createClient();
-  let { user } = (await supabase.auth.getUser()).data;
+  const { data: { user } } = await supabase.auth.getUser();
 
   // if user is signed in, redirect to chat
-  if (user && !user.is_anonymous) redirect('/chat');
-
-  // if user is not signed in, sign in anonymously
-  if (!user) user = (await supabase.auth.signInAnonymously()).data.user;
+  if (user && !user.is_anonymous) redirect('/chat/0');
 
   // if there is still no user, give up
   if (!user) return notFound();
 
   return (
-    <Chat
-      user_id={user.id}
-      persona_id="new"
-      persona_name="April"
-      initialMessages={[]}
-    />
+    <>  
+      <AppHeader title="Chat" subtitle="April" button={<SignInDialog/>} />
+      <Chat
+        user_id={user.id}
+        persona_id="new"
+        initialMessages={[]}
+      />
+    </>
   );
 }
