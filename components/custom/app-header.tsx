190472a45ef2a481283528aab3200ad345b7cef4
vincecloutier <94201611+vincecloutier@users.noreply.github.com>
2025-05-02 08:43:43 -0400

    first draft of user profile and app header with buttons

--- components/custom/app-header.tsx
+++ components/custom/app-header.tsx
@@ -2,9 +2,7 @@
 
 import { toast } from "sonner"
 import { useState, useEffect } from "react"
-import { useRouter} from "next/navigation"
-
-import {UserRound } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
@@ -13,12 +11,23 @@ import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
-import { signIn } from "@/lib/supabase/client";
+import { signIn, signOut } from "@/lib/supabase/client";
+import { UserRound } from "lucide-react";
+import GenericForm, { FieldSchema } from "@/components/custom/generic-form";
+import { saveProfile } from "@/lib/actions"
+import { Profile } from "@/lib/types";
+import { User } from "@supabase/supabase-js";
 
-export function AppHeader({title, subtitle, isAnonymous = false}: {title: string, subtitle: string, isAnonymous?: boolean})  {
-    const router = useRouter();
+const profileFields: FieldSchema[] = [ 
+  { name: 'name', label: 'Name', rowId: 'a1', type: 'text', required: true },
+  { name: 'birthday', label: 'Birthday', rowId: 'a2', type: 'calendar', required: true },
+  { name: 'telephone', label: 'Telephone', rowId: 'a3', type: 'tel', required: true },
+  { name: 'location', label: 'Location', rowId: 'a4', type: 'city', required: true },
+]
+
+export function AppHeader({title, subtitle, button}: {title: string, subtitle: string, button: React.ReactNode})  {
     return (
-        <header className="flex h-16 shrink-0 items-center justify-between gap-2 px-4">
+        <header className="relative top-0 left-0 right-0 flex h-16 shrink-0 items-center justify-between gap-2 px-4">
             <div className="flex items-center gap-2">
             <SidebarTrigger/>   
             <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
@@ -32,13 +41,12 @@ export function AppHeader({title, subtitle, isAnonymous = false}: {title: string
                 </BreadcrumbList>
             </Breadcrumb>
             </div>
-            {isAnonymous && (<SignInDialog/>)}
-            {!isAnonymous && (<Button variant="ghost" size="icon" className="h-7 w-7" onClick={() => router.push('/profile')}> <UserRound className="size-4"/> </Button>)}
+            {button}
         </header>
     )
 }
 
-function SignInDialog() {
+export function SignInDialog() {
   const [email, setEmail] = useState('');
   const [rememberEmail, setRememberEmail] = useState(false);
   const [open, setOpen] = useState(false);
@@ -96,5 +104,34 @@ function SignInDialog() {
       </form>
     </DialogContent>
     </Dialog>
+  )
+}
+
+
+export function UserProfile({user, profile}: {user: User, profile: Profile | null}) {
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  async function handleSignOut() {
+    await signOut();
+    router.push('/');
+    router.refresh();
+    setOpen(false);
+  }
+
+  return (  
+    <>
+      <Button variant="ghost" className="h-7 w-7" onClick={() => setOpen(true)}><UserRound size={16}/></Button>
+      <GenericForm
+        formTitle="Edit Profile"
+        formDescription="Update the details for your profile. Please note your location will only be used to provide your personas with localized time, weather, and search results."
+        fields={profileFields}
+        startingValues={profile || {id: user.id, attributes: {},  sender_address: ""}}
+        saveAction={saveProfile}
+        open={open}
+        onOpenChange={setOpen}
+        destructiveButton={<Button variant="outline" onClick={handleSignOut}>Sign Out</Button>}
+        forceAnswer={!profile}
+      />
+    </>
   )
 }
