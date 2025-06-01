e0cf45a520d2b54521cded1ca0ca38584a6d0fa2
vincecloutier <94201611+vincecloutier@users.noreply.github.com>
2025-06-01 18:14:11 -0400

    moved theme toggle out

--- components/custom/theme-provider.tsx
+++ components/custom/theme-provider.tsx
@@ -1,8 +1,21 @@
 "use client"
  
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
- 
+import { useTheme, ThemeProvider as NextThemesProvider } from "next-themes"
+import { Button } from "@/components/ui/button"
+import { Sun, Moon } from "lucide-react"
+
 export function ThemeProvider({children, ...props}: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme()
+  return (
+    <Button variant="ghost" className="size-7" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+      <Sun size={4} className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon size={4} className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  )
 }
