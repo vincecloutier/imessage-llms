d8e2c859be9fa2355cd899733b29cba2a6ad0b0d
vincecloutier <94201611+vincecloutier@users.noreply.github.com>
2025-06-02 12:10:37 -0400

    commented safari colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,10 +16,10 @@ export const viewport = { maximumScale: 1 };
 export default async function RootLayout({children, sidebar}: {children: React.ReactNode, sidebar: React.ReactNode}) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <meta name="theme-color" content="#000000" media="(prefers-color-scheme: light)" />
+      {/* <head>
+        <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
         <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)" />
-      </head>
+      </head> */}
       <body className="antialiased">
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
         <Toaster position="top-center" richColors/>
