807336763781c90b4d893e2e1ed420cea1806663
vincecloutier <94201611+vincecloutier@users.noreply.github.com>
2025-06-02 13:50:01 -0400

    better theme-switcher

--- components/custom/theme-provider.tsx
+++ components/custom/theme-provider.tsx
@@ -5,6 +5,7 @@ import { useTheme, ThemeProvider as NextThemesProvider } from "next-themes"
 import { Button } from "@/components/ui/button"
 import { Sun, Moon } from "lucide-react"
 import { SidebarMenuButton, SidebarMenuItem} from "../ui/sidebar"
+import { ThemeSwitcher } from "../ui/theme-switcher"
 
 export function ThemeProvider({children, ...props}: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
@@ -23,12 +24,5 @@ export function ThemeToggle() {
 
 export function ThemeToggleSidebar() {
   const { theme, setTheme } = useTheme()
-  return (
-    <SidebarMenuItem key="Theme Toggle" className="text-muted-foreground hover:text-foreground">
-      <SidebarMenuButton className="p-2 flex items-center justify-center" onClick={() => setTheme(theme === "dark" ? "light" : "dark")} tooltip={{ children: "Toggle Theme", hidden: false }}>
-        <Sun className="size-5 shrink-0 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <Moon className="size-5 shrink-0 absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      </SidebarMenuButton>
-    </SidebarMenuItem>
-  )
+  return <ThemeSwitcher className="bg-transparent rounded-none border-none" defaultValue="system" value={theme as "dark" | "light" | "system"} onChange={setTheme} />
 } 
