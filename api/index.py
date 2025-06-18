2d4a2dcc3a836a25eff23b03462162e8571637d1
vincecloutier <94201611+vincecloutier@users.noreply.github.com>
2025-06-18 07:26:35 -0400

    lets' try again

--- api/index.py
+++ api/index.py
@@ -1,22 +1,6 @@
 from flask import Flask
-from typing import Dict
-import time
-
 app = Flask(__name__)
 
-
-@app.route("/api/py/")
-def root() -> Dict[str, str]:
-    return {"message": "Hello World"}
-
-
-def some_work(input: str) -> str:
-    return f"processed '{input}'"
-
-
-
-
-@app.route("/api/py/get-data", methods=["POST"])
-def get_data() -> Dict[str, str]:
-    return {"message": "get data response"}
-
+@app.route("/api/python")
+def hello_world():
+    return "<p>Hello, World!</p>"
